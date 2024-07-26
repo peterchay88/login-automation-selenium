d7e1164eb5188e47befcc8938b9eb3587616c711
Peter Chay <peterchay88@gmail.com>
2024-07-26 15:32:33 -0600

    using implicit wait

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ def driver(request):
         my_driver = webdriver.Firefox()
     else:
         raise ValueError(f"Invalid value. Expected chrome or firefox but got {request.config.getoption('--browser')}")
+    my_driver.implicitly_wait(10)
     yield my_driver
     my_driver.close()
 
--- tests/exceptions/test_exceptions.py
+++ tests/exceptions/test_exceptions.py
@@ -1,9 +1,5 @@
-import time
-
 import pytest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.mark.exception_tc01
@@ -14,6 +10,5 @@ def test_find_row_2(driver):
     add_button = driver.find_element(By.ID, "add_btn")
     add_button.click()
     # Verify Row 2 is displayed
-    second_row = (WebDriverWait(driver, 10).
-                  until(EC.presence_of_element_located((By.XPATH, "//div[@id='row2']/input[@class='input-field']"))))
-    assert second_row.is_displayed()
+    second_row = driver.find_element(By.XPATH, value="//div[@id='row2']/input[@class='input-field']")
+    assert second_row.is_displayed(), "Error. Row two is not displayed on the web page"
