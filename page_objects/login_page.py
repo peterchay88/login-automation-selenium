05e46c2db352ec29d0c81503874146fa9eb12fd6
Peter Chay <peterchay88@gmail.com>
2024-07-31 14:02:56 -0600

    added doc strings to each method

--- page_objects/login_page.py
+++ page_objects/login_page.py
@@ -2,28 +2,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from page_objects.base_page import BasePage
 
 
-class LoginPage:
+class LoginPage(BasePage):
     __url = "https://practicetestautomation.com/practice-test-login/"
     __username_field = (By.ID, "username")
     __password_field = (By.NAME, "password")
     __submit_button = (By.XPATH, "//button[@class='btn']")
 
     def __init__(self, driver: WebDriver):
-        self.driver = driver
+        super().__init__(driver)
 
     def open_web_page(self):
-        self.driver.get(self.__url)
+        """
+        This method opens a web page
+        :return:
+        """
+        self._driver.get(self.__url)
 
     def execute_login(self, username: str, password: str):
-        wait = WebDriverWait(self.driver, 10)
-        # Type username student into username field
-        wait.until(EC.visibility_of_element_located(self.__username_field))
-        self.driver.find_element(self.__username_field). send_keys(username)
-        # Type password Password123 into password field
-        wait.until(EC.visibility_of_element_located(self.__password_field))
-        self.driver.find_element(self.__password_field).send_keys(password)
+        """
+        This method attempts to log in via the username and password passed as arguments
+        :param username: Username used during login
+        :param password: Password used during login
+        :return:
+        """
+        # Type into the username field
+        super()._type_into_element(locator=self.__username_field, text=username)
+        # Type into the password field
+        super()._type_into_element(locator=self.__password_field, text=password)
         # Push submit button
-        wait.until(EC.visibility_of_element_located(self.__submit_button))
-        self.driver.find_element(self.__submit_button).click()
+        super()._click(locator=self.__submit_button)
+
