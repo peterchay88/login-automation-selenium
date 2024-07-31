4cce59a7fa814e7174b4b8eee01a618f5c4df1e9
Peter Chay <peterchay88@gmail.com>
2024-07-31 14:02:46 -0600

    added doc strings to each method

--- page_objects/logged_in_successfully.py
+++ page_objects/logged_in_successfully.py
@@ -12,12 +12,24 @@ class LoggedInSuccessfully:
 
     @property
     def get_expected_url(self) -> str:
+        """
+        Returns the expected url for the web page when you log in successfully
+        :return:
+        """
         return self.__url
 
     @property
-    def get_header(self) -> str:
+    def get_header_text(self) -> str:
+        """
+        Returns the header text from the web page when you log in successfully
+        :return:
+        """
         return self.driver.find_element(self.__header_locator).text
 
     @property
     def is_logout_button_displayed(self) -> bool:
+        """
+        Checks to see if the logout button is displayed
+        :return:
+        """
         return self.driver.find_element(self.__log_out_button).is_displayed()
