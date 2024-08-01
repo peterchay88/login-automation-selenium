09d4cde462e921c9626f6d57b9e2fbd1502b8d1b
Peter Chay <peterchay88@gmail.com>
2024-08-01 17:30:09 -0600

    Added more selenium methods

--- page_objects/exceptions_page.py
+++ page_objects/exceptions_page.py
@@ -8,7 +8,9 @@ class ExceptionsPage(BasePage):
     __edit_button = (By.ID, "edit_btn")
     __add_button = (By.ID, "add_btn")
     __second_row_input = (By.XPATH, "//div[@id='row2']/input[@class='input-field']")
-    __save_button = (By.XPATH, "//div[@id='row2']/button[@id='save_btn']")
+    __first_row_input = (By.XPATH, "//div[@id='row1']/input[@class='input-field']")
+    __row_one_save_button = (By.XPATH, "//div[@id='row1']/button[@id='save_btn']")
+    __row_two_save_button = (By.XPATH, "//div[@id='row2']/button[@id='save_btn']")
     __remove_button = (By.ID, "remove_btn")
     __saved_confirmation_message = (By.ID, "confirmation")
 
@@ -29,22 +31,33 @@ class ExceptionsPage(BasePage):
         """
         super()._click(locator=self.__add_button)
 
+    def click_edit_button(self):
+        """
+        This Method clicks the edit button on the exceptions page
+        :return:
+        """
+        super()._click(locator=self.__edit_button)
+
+    def click_row_one_save_button(self):
+        """
+        This Method clicks the save button on row one on the exceptions page
+        :return:
+        """
+        super()._click(locator=self.__row_one_save_button)
+
+    def click_row_two_save_button(self):
+        """
+        This Method clicks the save button on row two on the exceptions page
+        :return:
+        """
+        super()._click(locator=self.__row_two_save_button)
+
     def is_row_two_displayed(self) -> bool:
         """
         This method checks to see if row 2 on the exceptions page is displayed
         :return:
         """
         return super()._is_displayed(locator=self.__second_row_input, time=6)
-
-    def add_to_list_of_favorite_foods(self, text):
-        """
-        This method will do the following:
-        Click Add button, Wait for second row to load, type into second input field, and push save
-        :return:
-        """
-        super()._click(locator=self.__add_button)
-        super()._type_into_element(locator=self.__second_row_input, text=text, time=6)
-        super()._click(locator=self.__save_button)
 
     def is_saved_confirmation_message_displayed(self) -> bool:
         """
@@ -53,3 +66,46 @@ class ExceptionsPage(BasePage):
         :return:
         """
         return super()._is_displayed(locator=self.__saved_confirmation_message)
+
+    def clear_text_in_row_one_input(self):
+        """
+        This method clears the text in the row 1 input field
+        :return:
+        """
+        super()._clear_text(locator=self.__first_row_input)
+
+    def get_text_from_confirmation_msg(self) -> str:
+        """
+        This method returns the text in the saved confirmation message on the exceptions page
+        :return:
+        """
+        return super()._get_element_text(locator=self.__saved_confirmation_message)
+
+    def get_text_from_row_one_input(self) -> str:
+        """
+        This method returns the text in the row one input field on the exceptions page
+        :return:
+        """
+        return super()._get_element_text(locator=self.__first_row_input)
+
+    def add_to_list_of_favorite_foods(self, text: str):
+        """
+        This method will do the following:
+        Click Add button, Wait for second row to load, type into second input field, and push save
+        :return:
+        """
+        self.click_add_button()
+        super()._type_into_element(locator=self.__second_row_input, text=text, time=6)
+        self.click_row_two_save_button()
+
+    def edit_row_one_input(self, text: str):
+        """
+        This method will do the following:
+        Click the edit button, clear the row one input field, enter new text in, and click save
+        :param text: New text to be entered
+        :return:
+        """
+        self.click_edit_button()
+        super()._clear_text(locator=self.__first_row_input)
+        super()._type_into_element(locator=self.__first_row_input, text=text)
+        self.click_row_one_save_button()
