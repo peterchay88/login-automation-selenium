074994b003ba9685b570493235f8b792fea4fd28
Peter Chay <peterchay88@gmail.com>
2024-08-01 00:03:55 -0600

    Updated test comments

--- tests/login/test_login_page_negative.py
+++ tests/login/test_login_page_negative.py
@@ -11,14 +11,22 @@ class TestNegative:
         pytest.param("student", "incorrectPassword", "Your password is invalid!", marks=pytest.mark.negative_2)
     ])
     def test_negative_login_username(self, username, password, expected_error_msg, driver):
+        """
+        This test confirms the correct error state if you enter the wrong username or password
+        when trying to log in
+        1. Open web page
+        2. Type defined username for test into the username field
+        3. Type defined password for test into the password field
+        4. Check if login error exists. Confirm that the error message is correct for the test.
+        :param username:
+        :param password:
+        :param expected_error_msg:
+        :param driver:
+        :return:
+        """
         login_page = LoginPage(driver)
-        # open web page
         login_page.open_browser()
-        # Type incorrectUser into Username field & Password123 into the password field. Click the submit button.
         login_page.execute_login(username=username, password=password)
-
-        # Verify the error message is displayed
         assert login_page.check_if_error_msg_exists(), "Error message does not exist on page"
-        # # Verify error message text is Your username is invalid!
         assert login_page.check_error_message_text() == expected_error_msg, \
             "Text in error message does not match 'Your username is invalid!'"
