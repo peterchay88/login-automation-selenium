5bc88b11974222f5d8089bd8f78746be77131513
Peter Chay <peterchay88@gmail.com>
2024-08-01 00:00:46 -0600

    Updated test comments

--- tests/login/test_login_page_positive.py
+++ tests/login/test_login_page_positive.py
@@ -9,21 +9,25 @@ class TestPositive:
 
     @pytest.mark.tcid01
     def test_login_positive(self, driver):
+        """
+        1. Navigate to web page
+        2. Type student into the username field
+        3. Type Password123 into the password field
+        4. Push the submit button
+        5. Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+        6. Verify that you are on the "logged in page" by confirming the logout button, page header, and url
+        :param driver:
+        :return:
+        """
         login_page = LoginPage(driver=driver)
-        # Navigate to web page
         login_page.open_browser()
-        # Type student into username field &  Password123 into password field. Then Push submit button
         login_page.execute_login(username="student", password="Password123")
 
-        # Verify new page URL contains practicetestautomation.com/logged-in-successfully/
         logged_in_page = LoggedInSuccessfully(driver=driver)
         assert logged_in_page.current_url == logged_in_page.get_expected_url, \
             (f"Unexpected value for current url. Expected {logged_in_page.get_expected_url}. "
              f"Actual {login_page.current_url}")
-        # After you are logged in verify that logged in successfully exists on the page
         assert logged_in_page.get_header_text == "Logged In Successfully", \
             f"Unexpected value for header text. Actual {logged_in_page.get_header_text}"
-        # Verify that the log-out button exists on the page
-
         assert logged_in_page.is_logout_button_displayed, "Error logout button is not displayed"
 
